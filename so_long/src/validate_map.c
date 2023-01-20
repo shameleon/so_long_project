67b445ff4c2d2888e0e18c1fb50c9735d08f4696
jmouaike <john.mouaikel@gmail.com>
2023-01-20 15:03:35 +0100

    reading from file, needs improvement

--- so_long/src/validate_map.c
+++ so_long/src/validate_map.c
@@ -1,32 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   validate_map.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmouaike <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/10 07:49:21 by jmouaike          #+#    #+#             */
-/*   Updated: 2023/01/10 07:53:39 by jmouaike         ###   ########.fr       */
+/*   Created: 2023/01/18 13:08:46 by jmouaike          #+#    #+#             */
+/*   Updated: 2023/01/18 13:09:11 by jmouaike         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG
-# define SO_LONG
 
-typedef struct s_map
-{
-	char	**map;
-}	t_map
-
-typedef struct s_game
-{
-	void	*mlx;
-	void	*win;
-}	t_game
-
-#include <stdlib.h>
-
-
-int main(int argc, char **argv);
-
-#endif
